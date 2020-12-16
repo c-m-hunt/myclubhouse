9b3e1dd9e68b7e2d31b89e324f34b3430fcab954
Chris Hunt <chris.hunt@hudl.com>
2020-12-16 17:37:12 +0000

    Add all the user fields

--- data/user.go
+++ data/user.go
@@ -4,18 +4,64 @@ import (
 	"fmt"
 )
 
-// User - Basic structure of a user
 type User struct {
-	//TODO(c-m-hunt): Add more fields as required
 	ID       int
 	Username string
 	Title    string
 	Forename string
 	Surname  string
+	Photo    string
+	Address  struct {
+		ID            int
+		Street        string
+		City          string
+		County        string
+		Country       string
+		Postcode      string
+		MapLocation   string
+		MapLatitude   float32
+		MapLongitude  float32
+		MailAddressee string
+	}
+	HomeTelephone     string
+	BusinessTelephone string
+	MobileTelephone   string
+	Email             string
+	Email2            string
+	Gender            string
+	AbilityLevel      struct {
+		ID    int
+		Name  string
+		Level int
+	}
+	AbilityLevelAchievedDate string
+	SecondAbilityLevel       struct {
+		ID    int
+		Name  string
+		Level int
+	}
+	SecondAbilityLevelAchievedDate string
+	DOB                            string
+	Age                            int
+	RegistrationDate               string
+	IsCurrentMember                bool
+	MembershipCategories           []string
+	SubscriptionResources          []string
+	GroupName                      string
+	MembershipNumber               string
+	MembershipExpiryDate           string
+	FamilyGroup                    string
+	Parent1                        string
+	Parent2                        string
+	TokenBalance                   int
+	LastLoginTime                  string
+	Notes                          string
+	Occupation                     string
+	Attributes                     struct{}
 }
 
 // FullName - Returns the full name of the user concatenating the
-// title, forename and surname
+// title forename and surname
 func (u User) FullName() string {
 	return fmt.Sprintf("%v %v %v", u.Title, u.Forename, u.Surname)
 }
