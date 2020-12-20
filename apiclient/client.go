9b9d838f3cd18c7e4b6adc3edd4a4e1b5eacaa2a
Chris Hunt <chris.hunt@hudl.com>
2020-12-20 15:09:23 +0000

    Remove redundant code

--- apiclient/client.go
+++ apiclient/client.go
@@ -5,32 +5,21 @@ import (
 	"net/http"
 )
 
-type Service interface {
-	GetData(RequestQuery, *interface{})
-}
-
-type UsersService struct{}
-type EventsService struct{}
-
 // Client - Api client struct
 type Client struct {
 	SubDomain   string
 	AccessToken string
 	BaseURL     string
 	HTTPClient  *http.Client
-	UsersService
-	EventsService
 }
 
 // MakeClient - Generates an api client. Pass in httpClient to override defaults
 func MakeClient(subDomain string, accessToken string, httpClient *http.Client) Client {
 	baseURL := fmt.Sprintf("https://%v.myclubhouse.co.uk/api/v1/", subDomain)
 	c := Client{
-		SubDomain:     subDomain,
-		AccessToken:   accessToken,
-		BaseURL:       baseURL,
-		UsersService:  UsersService{},
-		EventsService: EventsService{},
+		SubDomain:   subDomain,
+		AccessToken: accessToken,
+		BaseURL:     baseURL,
 	}
 	if httpClient == nil {
 		c.HTTPClient = &http.Client{}
