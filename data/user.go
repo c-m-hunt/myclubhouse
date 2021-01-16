9fcd3c79a081c113ecc4da2106134321ecec64f3
Chris Hunt <chris.hunt@hudl.com>
2021-01-16 17:16:03 +0000

    Use real date objects rather than strings

--- data/user.go
+++ data/user.go
@@ -2,7 +2,34 @@ package data
 
 import (
 	"fmt"
+	"time"
 )
+
+// Users A slice of user
+type Users []User
+
+// Time parsable from input json
+type Time struct {
+	time.Time
+}
+
+// UnmarshalJSON grab the time from input format to a time object
+func (t *Time) UnmarshalJSON(b []byte) error {
+	strDate := string(b)
+	layouts := map[int]string{
+		21: `"2006-01-02T15:04:05"`,
+		23: `"2006-01-02T15:04:05.0"`,
+		24: `"2006-01-02T15:04:05.00"`,
+		25: `"2006-01-02T15:04:05.000"`,
+	}
+	if strDate == "null" || len(strDate) == 0 {
+		return nil
+	}
+	layout := layouts[len(strDate)]
+	tParsed, _ := time.Parse(layout, strDate)
+	*t = Time{tParsed}
+	return nil
+}
 
 // User User data structure
 type User struct {
@@ -42,20 +69,20 @@ type User struct {
 		Level int
 	}
 	SecondAbilityLevelAchievedDate string
-	DOB                            string
+	DOB                            *Time
 	Age                            int
-	RegistrationDate               string
+	RegistrationDate               *Time
 	IsCurrentMember                bool
 	MembershipCategories           []string
 	SubscriptionResources          []string
 	GroupName                      string
 	MembershipNumber               string
-	MembershipExpiryDate           string
+	MembershipExpiryDate           *Time
 	FamilyGroup                    string
 	Parent1                        string
 	Parent2                        string
 	TokenBalance                   int
-	LastLoginTime                  string
+	LastLoginTime                  *Time
 	Notes                          string
 	Occupation                     string
 	Attributes                     struct{}
